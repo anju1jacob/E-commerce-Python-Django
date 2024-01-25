9c7075387b3349ebe89dbf13a69ea6d2c322ad0d
Anju Jacob <jacob.anju98@gmail.com>
2024-01-25 12:51:22 +0530

    product layout design added

--- ecommerce/products/urls.py
+++ ecommerce/products/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name="index"),
+    path('product_list', views.list_products, name="list_products")
 ]
