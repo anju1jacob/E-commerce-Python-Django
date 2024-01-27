11dbd0f7bc36ffdd4528950c481a4158b663c52e
Anju Jacob <jacob.anju98@gmail.com>
2024-01-27 16:40:32 +0530

    customer module functions started

--- ecommerce/customers/views.py
+++ ecommerce/customers/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from . models import Customer
+from django.contrib import messages
 
 # Create your views here.
 def show_account(request):
+    if request.POST and 'register' in request.POST:
+        try:
+            username=request.POST.get('username')
+            email=request.POST.get('email')
+            password=request.POST.get('password')
+            address=request.POST.get('address')
+            phone=request.POST.get('phone')
+            user=User.objects.create(username=username, email=email, password=password)
+            customer=Customer.objects.create(user=user, address=address, phone=phone)
+            return redirect('index')
+        except Exception as e:
+            messages.warning(request,'invalid creditional..')
     return render(request,'account.html')
--- ecommerce/products/urls.py
+++ ecommerce/products/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name="index"),
     path('product_list', views.list_products, name="list_products"),
-    path('product_details', views.detail_product, name="detail_product"),
+    path('product_details/<int:pk>', views.detail_product, name="detail_product"),
 
 ]
